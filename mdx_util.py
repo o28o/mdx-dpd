8a7be826382ba9b6e549e3f60850a731d2e1d30d
o-kv <kvserv@o28o.zhh>
2023-03-27 20:06:51 +0600

    new

---
+++ mdx_util.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# version: python 3.5
+
+import sys
+import re
+from file_util import *
+
+def get_definition_mdx(word, builder):
+    """根据关键字得到MDX词典的解释"""
+    content = builder.mdx_lookup(word)
+    if len(content) < 1:
+        fp = os.popen('python lemma.py ' + word)
+        word = fp.read().strip()
+        fp.close()
+        print("lemma: " + word)
+        content = builder.mdx_lookup(word)
+    pattern = re.compile(r"@@@LINK=([\w\s]*)")
+
+    all_links_content = ""
+    for found_link in content:
+        rst = pattern.match(found_link)
+        if rst is not None:
+            link = rst.group(1).strip()
+            content = builder.mdx_lookup(link)
+        str_content = ""
+        if len(content) > 0:
+            for c in content:
+                str_content += c.replace("\r\n","").replace("entry:/","")
+        all_links_content += str_content  # TODO string builder
+
+    injection = []
+    injection_html = ''
+    output_html = ''
+
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        # base_path = sys._MEIPASS
+        base_path = os.path.dirname(sys.executable)
+    except Exception:
+        base_path = os.path.abspath(".")
+            
+    resource_path = os.path.join(base_path, 'mdx')
+
+    file_util_get_files(resource_path, injection)
+
+    for p in injection:
+        if file_util_is_ext(p, 'html'):
+            injection_html += file_util_read_text(p)
+
+    #return [bytes(str_content, encoding='utf-8')]
+    output_html = all_links_content + injection_html
+    return [output_html.encode('utf-8')]
+
+def get_definition_mdd(word, builder):
+    """根据关键字得到MDX词典的媒体"""
+    word = word.replace("/","\\")
+    content = builder.mdd_lookup(word)
+    if len(content) > 0:
+        return [content[0]]
+    else:
+        return []
